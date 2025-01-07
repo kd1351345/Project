0b1317fb4a938938ccfeb29dae06382f3bda291e
kd1351345 <kd1351345@st.kobedenshi.ac.jp>
2025-01-07 14:42:38 +0900

    更新

--- Assets/ML-Agents/Kadai/Ball.cs
+++ Assets/ML-Agents/Kadai/Ball.cs
@@ -1,8 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     public Transform ground;
+    public float margin = 1.0f; // 端からのマージン
+    public float collisionCooldown = 2.0f; // クールダウン時間
+
+    private bool isCoolingDown = false; // クールダウン中かどうかのフラグ
 
     void Start()
     {
@@ -11,16 +16,24 @@ public class Ball : MonoBehaviour
 
     public void ResetPosition()
     {
-        float x = Random.Range(-ground.localScale.x / 2, ground.localScale.x / 2);
-        float z = Random.Range(-ground.localScale.z / 2, ground.localScale.z / 2);
+        float x = Random.Range(-ground.localScale.x / 2 + margin, ground.localScale.x / 2 - margin);
+        float z = Random.Range(-ground.localScale.z / 2 + margin, ground.localScale.z / 2 - margin);
         transform.position = new Vector3(x, 1f, z);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if ((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("wall")) && !isCoolingDown)
         {
             ResetPosition();
+            StartCoroutine(CollisionCooldown());
         }
+    }
+
+    private IEnumerator CollisionCooldown()
+    {
+        isCoolingDown = true; // クールダウン開始
+        yield return new WaitForSeconds(collisionCooldown); // クールダウン時間待機
+        isCoolingDown = false; // クールダウン終了
     }
 }
